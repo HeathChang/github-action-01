fd38a568f965397395c3181cde1f9a3a7f5dc04b
heath <jannhyunsoo@gmail.com>
2024-06-23 20:30:39 +0900

    git commit -m "add new workflow with action + gitignore"

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
-  },
+    plugins: [react()], test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.js',
+        exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '.{idea,git,cache,output,temp}/**', './src/config/**',],
+    },
 });
